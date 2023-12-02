ac41bfa08c04b799a418e107770dd54bc049bb6d
ahbabaziz5 <ahbabaziz5@gmail.com>
2023-12-02 21:34:00 +0500

    some more

--- src/app/component/ContactCards.js
+++ src/app/component/ContactCards.js
@@ -35,6 +35,9 @@ const ContactCards = () => {
    </div>
    <div className='font-bold text-4xl text-center mt-3 mb-6'><h3>We'd love to hear <span className='text-red-500'>from You</span></h3></div>
  <div className='flex justify-center '><ContactForm/></div>
+ <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.861201490728!2d67.05871387529788!3d24.902715643565674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f1cf5b96bf7%3A0x127e07e2a63ed513!2zSmFtaWEgTWVtb24gTWFzamlkIEJ
+ hbnR2YSBOYWdhciAo2YXbjNmF2YYg2YXYs9is2K8g2KjYp9mG2bnZiNinINmG2q_YsSk!5e0
+ !3m2!1sen!2s!4v1701530166592!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
 
--- src/app/component/ContactForm.js
+++ src/app/component/ContactForm.js
@@ -11,13 +11,45 @@ const ContactForm = () => {
             message:""
 
         } )
+
         function handleChange(e){
           const  name = e.target.name;
           const  value = e.target.value;
           setUser((prevUser)=> ({...prevUser,[name] : value}))
 
         }
+   const handleSubmit = async (e) => {
+        e.preventDefault();
 
+        try {
+            const response = await fetch('/api/contact', {
+                method:'POST',
+                headers:{"Content_Type":"application/json"},
+                body: JSON.stringify({
+                    username:user.username,
+                    email:user.email,
+                    phone:user.phone,
+                    message:user.message
+                })
+            })
+            // Set the status based on the response from the API route
+            if (response.status === 200) {
+                setUser({
+                    username: "",
+                    email: "",
+                    phone: "",
+                    message: ""
+                })
+                setStatus('success');
+            } else {
+                setStatus('error');
+            }
+
+        }catch (e) {
+            console.log(e)
+        }
+
+    }
 
   return (
    <form className='border-2 h-50 w-96  text-md pt-5 pb-5 py-5 px-5 rounded-md'>
@@ -36,13 +68,13 @@ const ContactForm = () => {
 
 <div>
     <label htmlFor='phone' className='mb-2'> Phone Number<br />
-    <input type='number' name='phone' id='number' value={user.phone} onChange={handleChange} className='border-2 w-80 py-1 mt-2 mb-2' />
+    <input type='number' name='phone' id='number' value={user.phone} onChange={handleChange} placeholder='Enter your phone number' className='border-2 w-80 py-1 mt-2 mb-2' />
     </label>
 </div>
 
 <div>
     <label htmlFor='message' className='mb-2'> Enter your message<br />
-    <textarea name='message' id='message' value={user.message} onChange={handleChange} className='border-2 w-80 h-28 py-1 mt-2 mb-2'/>
+    <textarea name='message' id='message' value={user.message} onChange={handleChange} placeholder='Enter your message' className='border-2 w-80 h-28 py-1 mt-2 mb-2'/>
     </label>
 </div>
 <button className='border-2 bg-black text-white rounded-2xl w-32 h-9 text-center mt-2 hover:bg-white hover:text-black'>Send Message</button>
